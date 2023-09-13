a208c51cf54916b6cdb15612a28bd18fe2426179
Matthew Lim <matlim200@gmail.com>
2023-09-13 16:35:02 -0700

    feat: change method location

--- app/facades/collection_facade.rb
+++ app/facades/collection_facade.rb
@@ -5,4 +5,13 @@ class CollectionFacade
       Collection.new(collection, i)
     end
   end
+
+  def receive_collection_cards(collection_id)
+    collections = MagicService.get_collection_cards(collection_id)
+    card_data = collections[:cards]
+
+    card_data.map.with_index(1) do |collection, i|
+      Card.new(collection, i)
+    end
+  end
 end
