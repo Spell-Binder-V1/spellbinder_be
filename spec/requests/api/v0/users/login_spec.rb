45256e132164f5468dfc7237d6d5243aeb63f4a6
Jeff Nelson <jpnelson85@gmail.com>
2023-09-18 15:00:44 -0500

    add post login logic and tests

--- spec/requests/api/v0/users/login_spec.rb
+++ spec/requests/api/v0/users/login_spec.rb
@@ -1,20 +1,21 @@
 require "rails_helper"
-
 RSpec.describe "POST /api/v0/login", type: :request do
   before :each do
-    valid_attributes = {username: 'Buff MagicKarp', email: 'level@gang', password: 'password'}
-    expect {
-      post api_v0_users_path, params: { user: valid_attributes }
-    }.to change(User, :count).by(1)    # allow_any_instance_of(ApplicationController).to receive(:session).and_return({ user_id: @user.id })
+    valid_attributes = { username: 'Buff MagicKarp', email: 'level@gang', password: 'password' }
+    User.create(valid_attributes)
   end
   describe "with valid credentials" do
-    
     it "logs in a user" do
-      valid_attributes = { username: 'Buff MagicKarp', password: 'password' }
-      post api_v0_login_path, params: { user: valid_attributes }
-
-      
+      valid_data = { username: 'Buff MagicKarp', password: 'password' }
+      post api_v0_login_path, params: valid_data
       expect(response).to have_http_status(:ok)
+      expect(response.content_type).to match(a_string_including("application/json"))
+    end
+
+    it "sad path - invalid credentials" do
+      invalid_data = { username: 'Buff MagicKarp', password: 'wrongpassword' }
+      post api_v0_login_path, params: invalid_data
+      expect(response).to have_http_status(:unauthorized)
       expect(response.content_type).to match(a_string_including("application/json"))
     end
   end
