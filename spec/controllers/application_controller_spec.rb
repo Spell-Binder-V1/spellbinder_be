7c56c99708e8b70660d49fc452276e6b78930df0
Jeff Nelson <jpnelson85@gmail.com>
2023-09-18 11:56:12 -0500

    add Gabe suggestion

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,14 +1,15 @@
 require "rails_helper"
 
-RSpec.describe "Application Controller", type: :controller do
+RSpec.describe "Application Controller", type: :feature do
   before :each do
     user = User.create!(username: "Buff MagicKarp", email: "buff@gmail.com", password: "password")
-    @request.session[:user_id] = user.id
+    # session[:user_id] = user.id
+    allow_any_instance_of(ApplicationController).to receive(:session).and_return({ user_id: user.id })
   end
 
   it "logs out because of inactivity and redirects to login" do
-    @request.session[:last_activity] = 31.minutes.ago
-    @controller = ApplicationController.new
+    session[:last_activity] = 31.minutes.ago
+    # @controller = ApplicationController.new
 
     get "/api/v0/decks"
 
@@ -18,7 +19,7 @@ RSpec.describe "Application Controller", type: :controller do
   end
 
   it "sad path: does not log out because of inactivity and doesn't redirect to login" do
-    @request.session[:last_activity] = 29.minutes.ago
+    session[:last_activity] = 29.minutes.ago
 
     get "/api/v0/decks"
 
@@ -26,3 +27,5 @@ RSpec.describe "Application Controller", type: :controller do
     expect(response).to eq(:ok)
   end
 end
+
+# allow_any_instance_of(ApplicationController).to receive(:session).and_return({ user_id: user.id })
