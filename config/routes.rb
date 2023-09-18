27363a0e7d78ba40ab8c651ce54701a2c35e6be3
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2023-09-18 15:50:41 -0500

    sort merge conflict

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v0 do
       resources :users, only: [:create], path: 'register'
-      resources :decks, only: [:create, :index, :show, :update, :destroy], path: 'decks' do 
+      resources :decks, only: [:create, :index, :show, :update, :destroy], path: 'decks' do
       post 'add_card', to: 'decks#add_card'
       post 'remove_card', to: 'decks#remove_card'
-      end
+    end
       resources :collections, only: [:index, :show]
       get 'cards/random', to: 'cards#random'
       resources :cards, only: [:show]
