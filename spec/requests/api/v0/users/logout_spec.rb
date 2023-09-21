edb3b9e210231f6dbc41ec56fd36b0cc22296b82
Jeff Nelson <jpnelson85@gmail.com>
2023-09-21 15:35:57 -0500

    update logout spec tests

--- spec/requests/api/v0/users/logout_spec.rb
+++ spec/requests/api/v0/users/logout_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe "DELETE /api/v0/login", type: :request do
   before :each do
-    valid_attributes = { username: 'Buff MagicKarp', email: 'level@gang', password: 'password' }
+    valid_attributes = { username: 'Buff MagicKarp', email: 'level@gang' }
     User.create(valid_attributes)
     valid_data = { username: 'Buff MagicKarp', password: 'password' }
     post api_v0_login_path, params: valid_data
