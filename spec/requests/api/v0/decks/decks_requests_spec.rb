285b28f0f90b3b224fa4fac8d5aaa173db2145a0
Jeff Nelson <jpnelson85@gmail.com>
2023-09-21 15:13:13 -0500

    update decks request spec tests

--- spec/requests/api/v0/decks/decks_requests_spec.rb
+++ spec/requests/api/v0/decks/decks_requests_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "decks create" do
   describe "POST deck" do
     context "with valid parameters it creates a deck" do
       scenario "creates a new deck" do
-        user = User.create!(username: 'Buff MagicKarp', email: 'level@gang', password: 'password')
+        user = User.create!(username: 'Buff MagicKarp', email: 'level@gang')
 
         allow_any_instance_of(ApplicationController).to receive(:session).and_return({ user_id: user.id })
 
@@ -13,28 +13,6 @@ RSpec.describe "decks create" do
         expect {
           post api_v0_decks_path, params: { deck: valid_attributes }
         }.to change(Deck, :count).by(1)
-      end
-  end
-
-    context "with valid parameters" do
-      it "creates a new deck" do
-        user = User.create!(username: 'Buff MagicKarp', email: 'level@gang', password: 'password')
-
-        allow_any_instance_of(ApplicationController).to receive(:session).and_return({ user_id: user.id })
-
-        valid_attributes = { name: 'Weak MagicKarp' }
-        expect {
-          post api_v0_decks_path, params: { deck: valid_attributes }
-        }.to change(Deck, :count).by(1)
-      end
-
-      it "returns a 201 status code" do
-        user = User.create!(username: 'Buff MagicKarp', email: 'level@gang', password: 'password')
-
-        allow_any_instance_of(ApplicationController).to receive(:session).and_return({ user_id: user.id })
-
-        valid_attributes = { name: 'Weak MagicKarp' }
-        post api_v0_decks_path, params: { deck: valid_attributes }
         expect(response).to have_http_status(201)
       end
     end
@@ -45,18 +23,13 @@ RSpec.describe "decks create" do
         expect {
           post api_v0_decks_path, params: { deck: invalid_attributes }
         }.not_to change(Deck, :count)
-      end
-
-      it "returns a 302 status code" do
-        invalid_attributes = { name: nil }
-        post api_v0_decks_path, params: { deck: invalid_attributes }
         expect(response).to have_http_status(302)
       end
     end
 
     context 'logged in, but invalid attributes' do
       scenario 'does not create a new deck' do
-        user = User.create!(username: 'Buff MagicKarp', email: 'level@gang', password: 'password')
+        user = User.create!(username: 'Buff MagicKarp', email: 'level@gang')
 
         allow_any_instance_of(ApplicationController).to receive(:session).and_return({ user_id: user.id })
 
