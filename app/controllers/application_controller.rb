c4aa2374f548204ac4de0dd87f00362651ee51bb
Jeff Nelson <jpnelson85@gmail.com>
2023-09-21 17:54:44 -0500

    final changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
-  before_action :check_activity, if: :current_user
+  # before_action :check_activity, if: :current_user
 
   def current_user
     @_current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -12,13 +12,13 @@ class ApplicationController < ActionController::Base
       # render json: { error: "You must be logged in to do that." }, status: :unauthorized
     end
 
-  def check_activity
-    if current_user && session[:last_activity] && session[:last_activity] < 30.minutes.ago
-      reset_session
-      # render json: { notice: "You have been logged out due to inactivity." }, status: :ok
-      redirect_to login_path
-    else
-      session[:last_activity] = Time.current
-    end
-  end
+  # def check_activity
+  #   if current_user && session[:last_activity] && session[:last_activity] < 30.minutes.ago
+  #     reset_session
+  #     # render json: { notice: "You have been logged out due to inactivity." }, status: :ok
+  #     redirect_to login_path
+  #   else
+  #     session[:last_activity] = Time.current
+  #   end
+  # end
 end
