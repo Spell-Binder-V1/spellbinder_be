4be15bbc1fc5e4f35ceca7005afa8f0ff70d916b
Matthew Lim <125940571+MatthewTLim@users.noreply.github.com>
2023-12-20 10:04:17 -0800

    update session helper

--- app/controllers/helpers/session_helper.rb
+++ app/controllers/helpers/session_helper.rb
@@ -1,7 +1,8 @@
 module SessionHelper
   def logged_in?
-    return if current_user
+    unless current_user
       redirect_back(fallback_location: api_v0_search_path)
       flash[:error] = "You must be logged in or registered to create a deck."
+    end
   end
-end 
+end
