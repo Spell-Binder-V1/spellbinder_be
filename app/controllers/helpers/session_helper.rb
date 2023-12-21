e345b4b83b400f312f65c5017b88c69f5003464c
Matthew Lim <125940571+MatthewTLim@users.noreply.github.com>
2023-12-21 09:58:22 -0800

    test3

--- app/controllers/helpers/session_helper.rb
+++ app/controllers/helpers/session_helper.rb
@@ -1,7 +1,7 @@
-# module SessionHelper
+module SessionHelper
 #   def logged_in?
 #     return if current_user
 #       redirect_back(fallback_location: api_v0_search_path)
 #       flash[:error] = "You must be logged in or registered to create a deck."
 #   end
-# end
+end
