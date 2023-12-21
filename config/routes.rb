cbfac26974a7ab6bbae40e2d2fb1751022f6c830
Matthew Lim <125940571+MatthewTLim@users.noreply.github.com>
2023-12-21 10:40:23 -0800

    test17

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v0 do
-      # resources :users, only: [:create], path: 'register'
-      # resources :decks, only: [:create, :index, :show, :update, :destroy], path: 'decks' do
-      # post 'add_card', to: 'decks#add_card'
-      # post 'remove_card', to: 'decks#remove_card'
+      resources :users, only: [:create], path: 'register'
+      resources :decks, only: [:create, :index, :show, :update, :destroy], path: 'decks' do
+      post 'add_card', to: 'decks#add_card'
+      post 'remove_card', to: 'decks#remove_card'
     end
       resources :collections, only: [:index, :show]
       get 'cards/random', to: 'cards#random'
       resources :cards, only: [:show]
       get 'search', to: 'search#search'
 
-      # post 'login', to: 'users#login'
-      # delete 'logout', to: 'users#logout'
-    # end
+      post 'login', to: 'users#login'
+      delete 'logout', to: 'users#logout'
+    end
   end
 end
